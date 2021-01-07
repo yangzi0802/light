7be969f4858a66346f5c7796357b58900ccc822c
yangfq <915717502@qq.com>
2021-01-07 15:47:17 +0800

    readme

--- light-member/src/main/java/com/light/member/MemberApplication.java
+++ light-member/src/main/java/com/light/member/MemberApplication.java
@@ -3,9 +3,11 @@ package com.light.member;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RefreshScope
 public class MemberApplication {
 
     public static void main(String[] args) {
